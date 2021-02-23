419db497fc4d46f9f2d5db6f31b796309dce1994
Azamat <azamattemirzhan@gmail,com>
2021-02-23 20:23:41 +0300

    sdfsad

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,59 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Main {
+    public static final int CARS_COUNT = 4;
+    public static void main(String[] args) throws InterruptedException {
+
+        final CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT);
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10),cyclicBarrier);
+        }
+
+
+
+
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+
+        }
+
+        try {
+            cyclicBarrier.await();
+            cyclicBarrier.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+        Thread.sleep(10000);
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+
+
+
+
+
+        System.out.println("Победитель гонки " + Car.hero);
+
+    }
 }
+
+
+
+
+
+
+
+
--- src/main/java/Stage.java
+++ src/main/java/Stage.java
@@ -1,2 +1,11 @@
-public class Stage {
+import java.util.concurrent.Semaphore;
+
+abstract class Stage {
+    protected int length;
+    protected String description;
+    Semaphore smp;
+    public String getDescription() {
+        return description;
+    }
+    public abstract void go(Car c);
 }
